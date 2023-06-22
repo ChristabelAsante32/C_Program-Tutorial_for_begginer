fdb68ca492d06bb6a0b4a50120413cff72a84a42
Christabel Appiah <asanteachristabel32@gmail.com>
2023-06-22 01:13:15 -0700

    nom.c

---
+++ nom.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: A program that prints name,dob,number
+ */
+int main(void)
+{
+	printf("Name: Asante Appiah Christabel");
+	printf("DOB:march");
+	printf("Number: +233242..");
+	return 0;
+}
